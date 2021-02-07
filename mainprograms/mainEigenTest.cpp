52560bad9c79c7ec1dc089af52ba1ceb9f064f69
karolinnneoc <karolinneoc@gmail>
2021-02-07 17:20:15 -0300

    Code linked with boost-lib

--- mainprograms/mainEigenTest.cpp
+++ mainprograms/mainEigenTest.cpp
@@ -4,9 +4,11 @@
 #include <math.h>
 #include <Eigen/Core>
 #include <Eigen/Dense>
+#include "boost/date_time/posix_time/posix_time.hpp"
 
 using namespace Eigen;
 using namespace std;
+using namespace boost::posix_time;
 
 int main ()
 {
@@ -14,6 +16,9 @@ int main ()
 
     // Testing Eigen matrix
     // Copied from: https://gitlab.com/libeigen/eigen/-/blob/master/doc/examples/QuickStart_example.cpp 
+
+    auto tsim1 = microsec_clock::local_time();
+    
     MatrixXd m(2,2); // matrix composed by doubles.
     m(0,0) = 3;
     m(1,0) = 2.5;
@@ -25,6 +30,10 @@ int main ()
     v(0) = 4;
     v(1) = v(0) - 1;
     cout << "Here is the vector v:\n" << v << endl;
+
+    auto tsim2 = microsec_clock::local_time();
+
+    cout << "time measured with boost: " << tsim2 - tsim1 << endl;
   
     return 0;
 }
